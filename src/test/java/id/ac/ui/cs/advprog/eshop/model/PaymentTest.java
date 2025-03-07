8e58ae5b269311d12909b6679b8db2ebb083c250
daf <daffa199naufal@gmail.com>
2025-03-07 09:30:26 +0700

    [REFACTOR] changed PaymentTest string output for COD payment to match the one in the model class

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -14,20 +14,16 @@ class PaymentTest {
 
     @BeforeEach
     void setUp() {
-        // Data valid untuk Voucher Code
         validVoucherData = new HashMap<>();
         validVoucherData.put("voucherCode", "ESHOP1234ABC5678");
 
-        // Data invalid untuk Voucher Code
         invalidVoucherData = new HashMap<>();
         invalidVoucherData.put("voucherCode", "INVALIDVOUCHER");
 
-        // Data valid untuk Cash On Delivery
         validCODData = new HashMap<>();
         validCODData.put("address", "Jl. Bintara No.1");
         validCODData.put("deliveryFee", "10000");
 
-        // Data invalid untuk Cash On Delivery (kosong/null)
         invalidCODData = new HashMap<>();
         invalidCODData.put("address", "");
         invalidCODData.put("deliveryFee", null);
@@ -48,7 +44,7 @@ class PaymentTest {
     @Test
     void testCreatePaymentWithValidCOD() {
         Payment payment = new Payment("3", "CASH_ON_DELIVERY", validCODData);
-        assertEquals("PENDING", payment.getStatus()); // Karena belum ada aturan SUCCESS
+        assertEquals("SUCCESS", payment.getStatus());
     }
 
     @Test
